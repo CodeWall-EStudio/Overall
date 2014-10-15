3dfafd0ac766217cba1b1f3ac3b3c18488daeede
katans <myhorde@vip.qq.com>
2014-10-15 23:15:18 +0800

    前端init

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+var gulp = require('gulp'),
+	uglify = require('gulp-uglify'),
+	replace = require('gulp-replace'),
+	watch = require('gulp-watch'),
+	htmlreplace = require('gulp-html-replace');
