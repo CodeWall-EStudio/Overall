621fb6f1ec9c2a2b5803206811b31c212fdb6f14
azrael <azrael@imatlas.com>
2014-11-17 01:25:01 +0800

    up

--- server/modules/file_helper.js
+++ server/modules/file_helper.js
@@ -58,10 +58,11 @@ exports.writeExcel = function(res, options) {
                     Logger.error('>>>file convert error: to xls: ', err);
                     return;
                 }
-                res.set({
-                    'Content-Type': 'application/vnd.ms-excel',
-                    'Content-Disposition': 'attachment; filename=' + encodeURI(options.name)
-                });
+                // res.set({
+                //     'Content-Type': 'application/vnd.ms-excel',
+                //     'Content-Disposition': 'attachment; filename=' + encodeURI(options.name)
+                // });
+                res.attachment(target);
                 res.sendfile(target);
             });
         }
